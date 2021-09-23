1333c93b96664dbd52b4c4284eb1e541569e515d
Dylan Reimer <dylanreimer@berkeley.edu>
2021-09-23 14:37:53 -0700

    sprite init

---
+++ sprite_1/.history/empty-example/sketch_20210921171859.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  
+  createCanvas(1000, 1000);
+  background(200, 0, 200);
+}
+
+function draw() {
+  // put drawing code here
+  
+}
---
+++ sprite_1/.history/empty-example/sketch_20210921180518.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(1000, 1000)
+  background(200, 200, 200)
+}
+
+function draw() {
+  fill(200, 100, 0)
+  ellipse(200, 200, 100)
+}
